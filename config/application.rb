c28f04812926f57c9af1e57b28c9fcf6f926cf21
Yano Kohei <o83184206@gmail.com>
2021-09-07 00:55:46 +0900

    Add:config_generator

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,11 @@ module Goenbako
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.generators do |g|
+      g.assets false
+      g.skip_routes true
+      g.helper false
+      g.test_framework false
+    end
   end
 end
