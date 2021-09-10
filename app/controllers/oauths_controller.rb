b574f2d5171bb3302ba7750e116e62c5e1ff8bc8
Yano Kohei <o83184206@gmail.com>
2021-09-10 19:58:05 +0900

    Add:認証後のユーザーページを簡易的に実装

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -10,13 +10,13 @@ class OauthsController < ApplicationController
       return
     end
     if @user = login_from(provider)
-      redirect_to root_path, notice: "#{provider.titleize}ログインしました。"
+      redirect_to user_path(@user), notice: "#{provider.titleize}ログインしました。"
     else
       begin
         @user = create_from(provider)
         reset_session
         auto_login(@user)
-        redirect_to root_path, notice: "#{provider.titleize}ログインしました。"
+        redirect_to user_path(@user), notice: "#{provider.titleize}ログインしました。"
       rescue
         redirect_to root_path, alert: "#{provider.titleize}ログインに失敗しました。"
       end
