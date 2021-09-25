0011a673e5e9c70d02cc52b48db579471ab08a9a
Yano Kohei <o83184206@gmail.com>
2021-09-25 09:11:39 +0900

    Add:vueでのホーム画面の初期設定

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -13,8 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
     render: h => h(App)
   }).$mount()
   document.body.appendChild(app.$el)
-
-  console.log(app)
 })
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  root 'static_pages#top'
+  root to: 'home#index'
+
   post 'oauth/callback', to: 'oauths#callback'
   get 'oauth/callback', to: 'oauths#callback'
   get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
   delete 'logout', to: 'user_sessions#destroy'
   resources :users, only: %i[index edit update show destroy]
 
+  get '*path', to: 'home#index'
 end
