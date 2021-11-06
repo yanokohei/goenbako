522c5a36222cbc97c80ab2499ffc86e1881ef0b6
Yano Kohei <o83184206@gmail.com>
2021-11-06 14:05:28 +0900

    Fix:コントローラ、ルーティング修正

--- app/controllers/api/letters_controller.rb
+++ app/controllers/api/letters_controller.rb
@@ -1,18 +1,12 @@
 class Api::LettersController < ApplicationController
   before_action :set_letter, only: [:edit :update, :destroy]
 
-  def index
-    @letters = Letter.all
-    render json: @letters
-  end
-
   def show
     render json: @letter
   end
 
   def create
-    @letter = Letter.new(letter_params)
-
+    @letter = current_user.letters.build(letter_params)
     if @letter.save
       render json: @letter
     else
@@ -37,12 +31,24 @@ class Api::LettersController < ApplicationController
     render json: @letter
   end
 
+  def send_letters
+    user = User.find(params[:id])
+    @send_letters = user.send_letters
+    render json: @send_letters
+  end
+
+  def received_letters
+    user = User.find(params[:id])
+    @received_letters = user.received_letters
+    render json: @received_letters
+  end
+
   private
   def set_letter
     @letter = current_user.letters.find(params[:id])
   end
 
   def letter_params
-    params.require(:letter).permit(:to_user_id, :past, :current, :future, :expect, :message)
+    params.require(:letter).permit(:past, :current, :future, :expect, :message).merge(sender_id: current_user.id, receiver_id: user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,12 @@ Rails.application.routes.draw do
       collection do
         get 'me'
       end
+      resources :letters
+      collection do
+        get :send_letters
+        get :received_letters
+      end
     end
-    resources :letters
   end
 
   get '*path', to: 'home#index'
