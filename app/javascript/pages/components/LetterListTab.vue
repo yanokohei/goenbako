0d13e39c0f6c1f55a578a4e1733e0ec614293d42
Yano Kohei <o83184206@gmail.com>
2021-11-08 15:30:52 +0900

    Add:レターリストタブをコンポーネント化

---
+++ app/javascript/pages/components/LetterListTab.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card>
+    <v-tabs
+      background-color="amber lighten-5"
+      centered
+      icons-and-text
+    >
+      <v-tabs-slider />
+
+      <v-tab
+        href="#tab-1"
+        class="mr-8"
+      >
+        <v-icon>mdi-email-fast-outline</v-icon>
+        受け取ったファンレター
+      </v-tab>
+      <v-tab href="#tab-3">
+        <v-icon>mdi-email-edit-outline</v-icon>
+        送ったファンレター
+      </v-tab>
+    </v-tabs>
+  </v-card>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+
+</style>
