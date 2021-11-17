1a331653f5abdf27e19a6917bba4bd7038a4432b
Yano Kohei <o83184206@gmail.com>
2021-11-17 14:11:14 +0900

    Add:ログイン情報をcookieに保存するように設定

--- app/javascript/store/index.js
+++ app/javascript/store/index.js
@@ -2,22 +2,28 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import users from './users'
 import createPersistedState from 'vuex-persistedstate'
-
+import Cookies from 'js-cookie';
 
 Vue.use(Vuex)
-
 
 export default new Vuex.Store({
   modules: {
     users,
   },
-  plugins: [createPersistedState(
-    {
-      key: 'goenbakoApp',
 
-      paths: [
-        'users.currentUser'
-      ]
-    }
-  )]
+  plugins: [
+    createPersistedState({
+      storage: {
+        key: 'goenbakoApp',
+
+        paths: [
+          'users.currentUser'
+        ],
+        getItem: key => Cookies.get(key),
+        setItem: (key, value) =>
+          Cookies.set(key, value, { expires: 14, secure: true }), // 14日間有効
+        removeItem: key => Cookies.remove(key),
+      },
+    }),
+  ],
 })
