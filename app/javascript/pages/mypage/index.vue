0555cb1a63cdb66eef4e710a8912c5b82a44038e
Yano Kohei <o83184206@gmail.com>
2021-11-18 17:02:13 +0900

    Add:マイページリンクコンポーネントの読み込み

--- app/javascript/pages/mypage/index.vue
+++ app/javascript/pages/mypage/index.vue
@@ -10,11 +10,28 @@
           class="white--text"
           rounded
           x-large
-          href="#"
+          @click="openShareLinkModal"
         >
           Myご縁箱をシェアする
         </v-btn>
       </v-col>
+      <v-col class="text-center mb-12">
+        <v-btn
+          color="grey"
+          class="white--text"
+          rounded
+          x-large
+          href="http://127.0.0.1:3000/users/2"
+        >
+          開発テスト用ユーザーページリンク
+        </v-btn>
+      </v-col>
+      <transition name="fade">
+        <ShareLinkModal
+          :is-visible-share-link-modal="isVisibleShareLinkModal"
+          @close-modal="handleCloseShareLinkModal"
+      />
+      </transition>
     <LetterListTab />
   </v-container>
 </template>
@@ -22,24 +39,36 @@
 <script>
 import { mapGetters } from "vuex"
 import ProfileCard from "../components/ProfileCard";
+import ShareLinkModal from "../components/ShareLinkModal";
 import LetterListTab from "../components/LetterListTab";
 
 export default {
   name: "MypageIndex",
   components: {
     ProfileCard,
+    ShareLinkModal,
     LetterListTab
   },
   data() {
     return {
-      user: {}
+      user: {},
+      isVisibleShareLinkModal: false,
     };
   },
   computed: {
     ...mapGetters({ currentUser: "users/currentUser" }),
   },
+  methods: {
+    openShareLinkModal() {
+      this.isVisibleShareLinkModal = true;
+    },
+    handleCloseShareLinkModal() {
+      this.isVisibleShareLinkModal = false;
+    },
+  },
   mounted() {
-    this.$axios.get("/users/me")
+// マイページ。ログイン後にアクセス
+    this.$axios.get("users/me")
     .then((res) => {
       this.user = res.data
       this.$store.commit('users/setCurrentUser', res.data)
