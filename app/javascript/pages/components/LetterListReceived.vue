0dd9bd5ba1c409f876369297cc4c9ee8b405b3ba
Yano Kohei <o83184206@gmail.com>
2021-11-18 16:55:13 +0900

    Add:受け取ったレターリストのコンポーネント作成

---
+++ app/javascript/pages/components/LetterListReceived.vue
@@ -0,0 +1,65 @@
+<template>
+<v-container>
+    <v-row class="ma-12" justify="center">
+      <v-card
+        flat
+
+        width="800px"
+        rounded="xl"
+      >
+        <v-card-title class="ps-16">
+         受け取ったレター／送ったレター
+        </v-card-title>
+
+      </v-card>
+<!-- お試し設置--------------------------->
+      <v-col class="text-center mt-8">
+        <v-btn
+          color="blue"
+          class="white--text"
+          x-large
+          @click="openShareLetterModal"
+        >
+        <v-icon>mdi-twitter</v-icon>
+          ツイートで共有する
+        </v-btn>
+      </v-col>
+    </v-row>
+    <transition name="fade">
+        <ShareLetterModal
+          :is-visible-share-letter-modal="isVisibleShareLetterModal"
+          @close-modal="handleCloseShareLetterModal"
+      />
+    </transition>
+  </v-container>
+</template>
+
+<script>
+import { mapGetters } from "vuex"
+import ShareLetterModal from "./ShareLetterModal";
+
+export default {
+  components: {
+    ShareLetterModal,
+  },
+  data() {
+    return {
+      isVisibleShareLetterModal: false,
+    };
+  },
+  computed: {
+    ...mapGetters({ currentUser: "users/currentUser" }),
+  },
+  methods: {
+    openShareLetterModal() {
+      this.isVisibleShareLetterModal = true;
+    },
+    handleCloseShareLetterModal() {
+      this.isVisibleShareLetterModal = false;
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- app/javascript/pages/components/LetterListTab.vue
+++ app/javascript/pages/components/LetterListTab.vue
@@ -1,30 +1,38 @@
 <template>
-  <v-card>
-    <v-tabs
-      background-color="amber lighten-5"
-      centered
-      icons-and-text
-    >
-      <v-tabs-slider />
-
-      <v-tab
-        href="#tab-1"
-        class="mr-8"
+  <v-container>
+    <v-card>
+      <v-tabs
+        background-color="amber lighten-5"
+        centered
+        icons-and-text
       >
-        <v-icon>mdi-email-fast-outline</v-icon>
-        受け取ったファンレター
-      </v-tab>
-      <v-tab href="#tab-3">
-        <v-icon>mdi-email-edit-outline</v-icon>
-        送ったファンレター
-      </v-tab>
-    </v-tabs>
-  </v-card>
+        <v-tabs-slider />
+
+        <v-tab
+          href="#tab-1"
+          class="mr-8"
+        >
+          <v-icon>mdi-email-fast-outline</v-icon>
+          受け取ったファンレター
+        </v-tab>
+        <v-tab href="#tab-3">
+          <v-icon>mdi-email-edit-outline</v-icon>
+          送ったファンレター
+        </v-tab>
+      </v-tabs>
+    </v-card>
+    <LetterListReceived />
+  </v-container>
 </template>
 
 <script>
-  export default {
+import LetterListReceived from './LetterListReceived';
+
+export default {
+  components: {
+    LetterListReceived,
   }
+}
 </script>
 
 <style>
