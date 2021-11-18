12c68ce3f686568630120a25bdfd218ea508ab75
Yano Kohei <o83184206@gmail.com>
2021-11-18 16:50:42 +0900

    Fix:モデル、ルーティングのミスを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
 
   has_many :authentications, dependent: :destroy
+
   has_many :letters, foreign_key: :sender_id, dependent: :destroy
-  has_many :sent_letters, through: :letters, source: :received_letter
+  has_many :sent_letters, through: :letters, source: :receiver #lettersテーブルを経由してreveive_id列を参照する
 
   has_many :receivers, class_name: "Letter", foreign_key: :receiver_id, dependent: :destroy
-  has_many :received_letters, through: :receivers, source: :sent_letter
+  has_many :received_letters, through: :receivers, source: :sender # receiversテーブルを経由してsender_idを参照する
 
   accepts_nested_attributes_for :authentications
 
