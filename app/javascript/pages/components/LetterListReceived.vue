85e8f0099960355a37d0d5f802ad4a55962f977a
Yano Kohei <o83184206@gmail.com>
2021-11-24 07:21:24 +0900

    Add:受け取ったレターリストコンポーネント

--- app/javascript/pages/components/LetterListReceived.vue
+++ app/javascript/pages/components/LetterListReceived.vue
@@ -1,34 +1,50 @@
 <template>
 <v-container>
-    <v-row class="ma-12" justify="center">
+    <v-row class="ma-8" justify="center">
       <v-card
         flat
-
+        color="amber lighten-5"
         width="800px"
         rounded="xl"
       >
         <v-card-title class="ps-16">
-         受け取ったレター／送ったレター
+          <router-link :to="{ name: 'UserIndex', params: { id: user.id }}">
+            <v-list-item-avatar size="50">
+              <img :src="user.image">
+            </v-list-item-avatar>
+          </router-link>
+            <v-list-item-content>
+              <v-list-item-title class="font-bold">{{ user.name }}</v-list-item-title>
+              <v-list-item-subtitle>
+                @{{ user.twitter_id }}
+                <v-btn icon color="blue" :href="twitterUrl">
+                  <v-icon>mdi-twitter</v-icon>
+                </v-btn>
+              </v-list-item-subtitle>
+            </v-list-item-content>
         </v-card-title>
 
-      </v-card>
-<!-- お試し設置--------------------------->
-      <v-col class="text-center mt-8">
+        <LetterItem
+          :letter-items="letterItems"
+          :user="user"
+        />
+              <v-col class="text-center">
         <v-btn
           color="blue"
           class="white--text"
-          x-large
+          small
           @click="openShareLetterModal"
         >
         <v-icon>mdi-twitter</v-icon>
-          ツイートで共有する
+          シェア
         </v-btn>
       </v-col>
+      </v-card>
     </v-row>
     <transition name="fade">
-        <ShareLetterModal
-          :is-visible-share-letter-modal="isVisibleShareLetterModal"
-          @close-modal="handleCloseShareLetterModal"
+      <ShareLetterModal
+        :is-visible-share-letter-modal="isVisibleShareLetterModal"
+        @close-modal="handleCloseShareLetterModal"
       />
     </transition>
   </v-container>
@@ -36,19 +52,34 @@
 
 <script>
 import { mapGetters } from "vuex"
+import LetterItem from '../components/LetterItem';
 import ShareLetterModal from "./ShareLetterModal";
 
 export default {
   components: {
     ShareLetterModal,
+    LetterItem
   },
   data() {
     return {
       isVisibleShareLetterModal: false,
     };
   },
+  props: {
+    user: {
+      type: Object,
+      required: true
+    },
+    letterItems: {
+      type: Object,
+      required: true
+    },
+  },
   computed: {
     ...mapGetters({ currentUser: "users/currentUser" }),
+    twitterUrl() {
+      return `https://twitter.com/${this.currentUser.twitter_id}`
+    }
   },
   methods: {
     openShareLetterModal() {
