6f3eeae0462221c5c808d5c1fddf88bdd34ca489
Yano Kohei <o83184206@gmail.com>
2021-12-09 15:31:16 +0900

    Add:URLのIDをtwitter_idに変更[vue]

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     get 'oauth/callback', to: 'oauths#callback'
     get 'oauth/:provider', to: 'oauths#oauth', as: :auth_at_provider
     delete 'logout', to: 'user_sessions#destroy'
-    resources :users, only: %i[index edit update show destroy] do
+    resources :users, param: :twitter_id, only: %i[index edit update show destroy] do
       collection do
         get 'me'
       end
