c80956393f40c35f186426cc3f996f81f55e0d0d
Yano Kohei <o83184206@gmail.com>
2021-12-13 20:01:53 +0900

    Add:heroku_push_test

--- app/controllers/crawlers_controller.rb
+++ app/controllers/crawlers_controller.rb
@@ -1,18 +1,7 @@
 class CrawlersController < ApplicationController
 
   def show
-    # current_user
     @share_letter_image = url("/img/mybox/jpg")
     # public/img/mybox.jpgをツイート時のデフォルトのOGP画像として反映させること・動的に変更する条件分岐
   end
-
-  private
-
-  # 参考
-
-  # def set_arrangement
-  #   @arrangement = Arrangement.find(decode_id(params[:id]))
-  # rescue StandardError => e
-  #   render_404(e)
-  # end
 end
