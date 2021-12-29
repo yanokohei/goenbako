557d5f2994cf98fb8bc70fde7e565459d2e32aa6
Yano Kohei <o83184206@gmail.com>
2021-12-29 12:46:08 +0900

    ShareImageモデルを実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_071638) do
+ActiveRecord::Schema.define(version: 2021_12_27_201446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 2021_10_31_071638) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["sender_id"], name: "index_letters_on_sender_id"
+  end
+
+  create_table "share_images", force: :cascade do |t|
+    t.bigint "letter_id", null: false
+    t.string "topic", null: false
+    t.text "image_url", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["letter_id"], name: "index_share_images_on_letter_id"
   end
 
   create_table "users", force: :cascade do |t|
