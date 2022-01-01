2290f8b70701d8d195b6b1386b830ef3fc5545a4
Yano Kohei <o83184206@gmail.com>
2022-01-01 20:50:40 +0900

    Add:layout Vue側

--- app/javascript/components/shared/TheBottomNavigation.vue
+++ app/javascript/components/shared/TheBottomNavigation.vue
@@ -9,6 +9,22 @@
     </v-btn>
 
     <v-btn
+      :to="{ name: 'User', params: { twitter_id: this.random_id }}"
+    >
+      <span>Random</span>
+
+      <v-icon>mdi-paw</v-icon>
+    </v-btn>
+
+    <v-btn
+      :to="{ name: 'Mypage' }"
+    >
+      <span>Search</span>
+
+      <v-icon>mdi-account-search-outline</v-icon>
+    </v-btn>
+
+    <v-btn
       to="/api/logout"
       data-method="delete"
       @click="logoutUser"
@@ -22,15 +38,41 @@
 
 <script>
 import Cookies from 'js-cookie';
+import axios from "axios";
 
 export default {
   name: "TheBottomNavigation",
+  data() {
+    return{
+      random_id: ''
+    }
+  },
+  mounted() {
+    this.fetchRandomUser()
+  },
+  computed: {
+    currentPath() {
+      return this.$route.path
+    },
+  },
   methods: {
     logoutUser() {
-      this.$store.commit('setCurrentUser', { user:null })
+      this.$store.commit('setCurrentUser', { user: null })
       Cookies.remove('vuex');
     },
-  }
+    async fetchRandomUser() {
+      do { await axios.get("/api/random")
+        .then((res) => {
+          this.random_id = res.data
+        })
+      } while (this.$route.path === `/${this.random_id}`);
+    }
+  },
+  watch: {
+    currentPath: function() {
+      this.fetchRandomUser()
+    }
+  },
 }
 </script>
 <style scoped>
