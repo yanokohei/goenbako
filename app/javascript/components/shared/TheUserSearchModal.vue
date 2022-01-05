ddbf25d94dddea13ecffcefc9fc9a4da8445c7c2
Yano Kohei <o83184206@gmail.com>
2022-01-05 21:42:14 +0900

    Add:TheUserSearchModal(ユーザー検索モーダル)

--- app/javascript/components/shared/TheUserSearchModal.vue
+++ app/javascript/components/shared/TheUserSearchModal.vue
@@ -1,59 +1,83 @@
 <template>
-  <v-row jusify-center>
-    <v-dialog
-      v-model="isVisibleUserSearchModal"
-      max-width="500"
-      @click:outside="handleCloseModal"
-    >
-      <v-card color="amber lighten-5">
-        <h3 class="ma-4">
-          <span class="my-5">ユーザー検索</span>
-          <v-divider />
-        </h3>
-        <v-card-text class="mt-5">
-          <v-text-field
-            background-color="#FFFFF0"
-            v-model="searchID"
-            label="TwitterID検索"
-            persistent-hint
-            outlined
-            dense
-            placeholder="@を省略して入力ください"
-            class="ma-0 mb-0 pb-0"
+  <v-dialog
+    v-model="isVisibleUserSearchModal"
+    max-width="500"
+    @click:outside="handleCloseModal"
+  >
+    <v-card color="amber lighten-5" height="450">
+      <h3 class="ma-4">
+        <span class="my-5">ユーザー検索</span>
+        <v-divider />
+      </h3>
+      <v-card-text class="mt-5 mb-1">
+        <v-text-field
+          background-color="#FFFFF0"
+          v-model="searchID"
+          label="TwitterID検索"
+          persistent-hint
+          outlined
+          dense
+          placeholder="@を省略して入力してください"
+          class="ma-0 mb-0 pb-0"
+        />
+        <v-card-actions class="justify-center pt-0">
+          <v-btn
+            small
+            @click="searchUser"
+          >
+            <v-icon>mdi-magnify</v-icon>検索
+          </v-btn>
+        </v-card-actions>
+        <transition name="fade">
+          <div class="xs-font mb-4 mt-4 text-center" v-if="this.missingUserSearch">
+            入力内容に誤りがあるか、<br>
+            まだご縁箱を開設していないユーザーのようです。
+          </div>
+        </transition>
+        <transition name="fade">
+          <TheInviteUser
+            v-if="currentUser && theInviteUser"
+            :search-id="searchID"
           />
-          <v-card-actions class="justify-center pt-0">
-            <v-btn
-              small
-              @click="searchUser"
-            >
-              <v-icon>mdi-magnify</v-icon>検索
-            </v-btn>
-          </v-card-actions>
-        </v-card-text>
-        <v-row
-          justify="center"
-          class="ma-8"
-        >
-          <v-card-actions>
-            <v-btn
-              large
-              @click="handleCloseModal"
-            >
-              閉じる
-            </v-btn>
-          </v-card-actions>
-        </v-row>
-      </v-card>
-    </v-dialog>
-  </v-row>
+        </transition>
+      </v-card-text>
+      <v-row v-if="this.beforeUserSearch" justify="center">
+        <div class="mt-8 text-center">
+          あなたがファンレターを書きたい<br>
+          \ ユーザーを検索してみましょう！ /
+        </div>
+        <v-img
+          src="/img/goenbako_smile.png"
+          max-width="220px"
+        />
+      </v-row>
+      <v-row
+        justify="center"
+        class="ma-8"
+      >
+        <v-card-actions>
+          <v-btn
+            large
+            @click="handleCloseModal"
+          >
+            閉じる
+          </v-btn>
+        </v-card-actions>
+      </v-row>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
 import axios from "axios";
 import { mapGetters } from "vuex"
+import TheInviteUser from "./TheInviteUser";
 
 export default {
   name: "TheUserSearchModal",
+  components: {
+    TheInviteUser
+  },
   props: {
     isVisibleUserSearchModal: {
       type: Boolean,
@@ -64,6 +88,9 @@ export default {
     return {
       searchID: "",
       result: "",
+      beforeUserSearch: true,
+      missingUserSearch: false,
+      theInviteUser: false
     };
   },
   computed: {
@@ -92,26 +119,41 @@ export default {
               type: "error",
               message: "検索に失敗しました。"
             })
+            this.beforeUserSearch = false
+            this.missingUserSearch = true
+            this.theInviteUser = true
           }
         })
       }
     },
     handleCloseModal() {
+      this.searchID = "";
+      this.beforeUserSearch = true
+      this.missingUserSearch = false
+      this.theInviteUser = false
       this.$emit("close-modal");
-    },
-    twitterShare() {
-      const url = `${location.origin}/${this.currentUser.twitter_id}`
-      return `https://twitter.com/share?text=${this.currentUser.name}さんのご縁箱です！%0aファンレターを書いてみよう！&url=${url}&hashtags=ご縁箱`;
     },
   }
 };
 </script>
 
 <style scoped>
+.xs-font{
+  font-size: 0.8em;
+  font-weight: bold;
+  line-height: 1;
+  color: #2c281e;
+}
 .modal {
   display: block;
 }
-.letter-item-input {
-  color: rgb(27, 21, 21)
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-to, .fade-leave {
+  opacity: 3;
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 2s;
 }
 </style>
