8ade6cc5f15d6811f3c29849cfba975f0e0b3424
Yano Kohei <o83184206@gmail.com>
2022-01-06 12:50:30 +0900

    Add:[Rails側]share_image

--- app/controllers/crawlers_controller.rb
+++ app/controllers/crawlers_controller.rb
@@ -2,5 +2,7 @@ class CrawlersController < ApplicationController
 
   def show
     @user = User.find_by!(twitter_id: params[:twitter_id])
+    share_image = ShareImage.find_by!(id: params[:id])
+    @share_image = share_image.image_url
   end
 end
