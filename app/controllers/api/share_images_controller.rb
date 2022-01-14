18536f8788c3a11d47900cd8d5e1343c7fe54690
Yano Kohei <o83184206@gmail.com>
2022-01-14 09:41:21 +0900

    carrierwave_error

--- app/controllers/api/share_images_controller.rb
+++ app/controllers/api/share_images_controller.rb
@@ -1,25 +1,26 @@
 class Api::ShareImagesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  def create
-    @share_image = ShareImage.new(share_image_params)
-    base64_url = @share_image.image_url['data:image/png;base64,'.length..-1]
-    decoded_data = Base64.decode64(base64_url)
-    filename = Time.zone.now.to_s + '.' + 'png'
-    File.open("#{Rails.root}/tmp/#{filename}", 'wb') do |f|
-      f.write(decoded_data)
-      @share_image.image_url = f
-    end
-    if @share_image.save
-      render json: @share_image, status: :ok
-    else
-      render json: @share_image, status: :bad_request
-    end
-  end
+  # def create
+  #   @share_image = ShareImage.new(share_image_params)
+  #   base64_url = @share_image.image_url['data:image/png;base64,'.length..-1]
+  #   decoded_data = Base64.decode64(base64_url)
+  #   filename = Time.zone.now.to_s + '.' + 'png'
+  #   File.open("#{Rails.root}/tmp/#{filename}", 'wb') do |f|
+  #     f.write(decoded_data)
+  #     @share_image.image_url = f
+  #   end
+  #       binding.pry
+  #   if @share_image.save
+  #     render json: @share_image, status: :ok
+  #   else
+  #     render json: @share_image, status: :bad_request
+  #   end
+  # end
 
-  private
+  # private
 
-    def share_image_params
-      params.require(:share_image).permit(:letter_id, :topic, :image_url)
-    end
+  #   def share_image_params
+  #     params.require(:share_image).permit(:letter_id, :topic, :image_url)
+  #   end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,16 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+# require 'carrierwave/storage/abstract'
+# require 'carrierwave/storage/file'
+# require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-  config.storage :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_directory = 'goenbako'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
-    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-    region: 'ap-northeast-1',
-    path_style: true
-  }
-end
+# CarrierWave.configure do |config|
+#   config.storage :fog
+#   config.fog_provider = 'fog/aws'
+#   config.fog_directory = 'goenbako'
+#   config.fog_credentials = {
+#     provider: 'AWS',
+#     aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+#     aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+#     region: 'ap-northeast-1',
+#     path_style: true
+#   }
+# end
