0c9511f542c487bda0e7dea2aa82a768922413e0
Yano Kohei <o83184206@gmail.com>
2022-01-16 05:54:05 +0900

    layout 開設リクエストの文言_端末によっては意図しない改行が発生する

--- app/javascript/components/shared/TheInviteUser.vue
+++ app/javascript/components/shared/TheInviteUser.vue
@@ -3,11 +3,11 @@
     <v-row justify="end">
       <v-img
         src="/img/writing.png"
-        max-width="40px"
+        max-width="35px"
       />
     </v-row>
-    <div class="xs-font mt-4 mb-4">
-      \ ご縁箱の開設をリクエストしてみませんか? /
+    <div class="xs-font mt-4 mb-2">
+      \ ご縁箱の開設をお願いしてみませんか? /
     </div>
     <v-btn
       color="#FCF6FF"
