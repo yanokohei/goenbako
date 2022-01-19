6138988462b5d666a3d0bab2d1ceecf4866a700f
Yano Kohei <o83184206@gmail.com>
2022-01-19 12:15:00 +0900

    Fix:画像の参照方法を修正

--- app/controllers/letter_shares_controller.rb
+++ app/controllers/letter_shares_controller.rb
@@ -1,7 +1,6 @@
 class LetterSharesController < ApplicationController
   def show
     @user = User.find_by!(twitter_id: params[:twitter_id])
-    share_image = ShareImage.find(params[:id])
-    @share_image = share_image.image_url
+    @share_image = ShareImage.find(params[:id])
   end
 end
