093a569a20dc05267894f40a3501172ba3c44a68
Yano Kohei <o83184206@gmail.com>
2022-01-22 17:47:15 +0900

    addLetterTopicToSvgが実行されない問題2

--- app/javascript/components/ShareLetterModal.vue
+++ app/javascript/components/ShareLetterModal.vue
@@ -131,6 +131,10 @@ export default {
       if (letterTitle.topic === "expect") {
         this.title = this.shortReceiverNameExpectCase()
       }
+      this.$store.dispatch("flash/setFlash", {
+        type: "success",
+        message: "関数が実行されました。"
+      })
     },
     postImage(letterTitle) {
       this.shareImage.letter_id = this.receivedLetter.letter.id
@@ -162,7 +166,6 @@ export default {
         const svgData = new XMLSerializer().serializeToString(svgElement);
         image.src = "data:image/svg+xml;charset=utf-8;base64," + btoa(unescape(encodeURIComponent(svgData)));
       };
-      this.addLetterTopicToSvg(letterTitle)
       this.addLetterTopicToSvg(letterTitle).then(() =>
         createCanvasFromSvgAndConversionPngUrl(this.$refs.svgArea, data => {
           this.shareImage.image_url = data
