82382135e9431c87c45b05ae086b5084306a7f80
Yano Kohei <o83184206@gmail.com>
2022-01-24 21:21:54 +0900

    Add:S3のアップロード先を環境ごとに分ける

--- app/uploaders/share_image_uploader.rb
+++ app/uploaders/share_image_uploader.rb
@@ -5,7 +5,11 @@ class ShareImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  if Rails.env.development? || Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
