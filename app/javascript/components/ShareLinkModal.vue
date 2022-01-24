61e87d9d55c076e8a86cd1db8eba7ccd2d849e64
Yano Kohei <o83184206@gmail.com>
2022-01-24 17:17:30 +0900

    layout ShareLinkModalデザイン

--- app/javascript/components/ShareLinkModal.vue
+++ app/javascript/components/ShareLinkModal.vue
@@ -6,29 +6,29 @@
   >
     <v-card color="amber lighten-5">
       <v-card-title>
-        <v-img max-width="23" src="/img/svg_parts.svg" />
+        <v-img max-width="21" src="/img/svg_parts.svg" />
         <span class="my-2 ml-2 m-font nowrap">  Myご縁箱をシェアする</span>
       </v-card-title>
       <div align="center">
         <v-img max-width="270" height="5" class="tranceparent" src="/img/en_line_top.svg" />
       </div>
       <v-img max-width="55" class="letter-position tranceparent" src="/img/triple_letter.svg" />
-      <v-col class="text-center mt-8">
+      <v-col class="text-center mt-4">
         <v-btn
           color="blue"
           class="white--text s-font"
-          large
+          small
           :href="twitterShare()"
         >
           <v-icon>mdi-twitter</v-icon>
           ツイートで共有する
         </v-btn>
       </v-col>
-      <v-col class="text-center mt-6">
+      <v-col class="text-center mt-4">
         <v-btn
           class="s-font"
           color="#FFF0E2"
-          large
+          small
           @click.prevent="copyLink"
           @click="snackbar = true"
         >
@@ -56,12 +56,12 @@
       </v-col>
       <v-row
         justify="center"
-        class="ma-8 py-3"
+        class="ma-4 py-2"
       >
         <v-img max-width="270" height="6" class="tranceparent mb-1" src="/img/en_line_under.svg" />
         <v-card-actions>
           <v-btn
-            large
+            small
             @click="handleCloseModal"
           >
             閉じる
@@ -133,7 +133,7 @@ export default {
 }
 .letter-position{
   position: absolute;
-  top: 250px;
+  top: 180px;
   left: 255px;
 }
 .nowrap{
