06ad43b6a711e499dd92f8fc9e552e39fe2e8792
Yano Kohei <o83184206@gmail.com>
2022-01-26 17:52:45 +0900

    layout NotFound 画像の大きさと幅調整

--- app/javascript/pages/NotFound.vue
+++ app/javascript/pages/NotFound.vue
@@ -1,26 +1,29 @@
 <template>
   <div align="center" class="mt-10">
     <h1>404 NotFound</h1>
-    <div class="mt-10 m-font">
+    <div class="mt-4 mb-8 m-font">
       <p>お探しのページが見つかりませんでした。</p>
       <p>
         一時的にアクセスができない状況にあるか
         <br>移動もしくは削除された可能性があります。
       </p>
     </div>
-    <v-card max-width="230" color="transparent" outlined>
-    <v-row justify="center" align="center">
-      <v-col class="ma-0 pa-0">
-        <v-img
-          class="tranceparent"
-          max-width="200"
-          src="/img/ken_shun.jpg"
-        />
-      </v-col>
-      <v-col>
-        <p class="position xs-font">しゅん...</p>
-      </v-col>
-    </v-row>
+    <v-card color="transparent" max-width="320" outlined>
+      <div justify="center" align="center">
+        <v-col class="ma-0 pa-0">
+          <v-img
+            class="tranceparent"
+            max-width="300"
+            min-height="330"
+            src="/img/NotFound.jpg"
+          />
+          <div class="text-center" align="center">
+            <p class="position xs-font">しゅん...</p>
+          </div>
+        </v-col>
+        <!-- <v-col class="ma-0 pa-0">
+        </v-col> -->
+      </div>
     </v-card>
     <v-col class="text-center">
       <v-btn
@@ -59,8 +62,8 @@ export default {
 }
 .position {
   position: absolute;
-  top: 120px;
-  left: 140px;
+  top: 190px;
+  left: 210px;
 }
 .tranceparent{
   mix-blend-mode: multiply;
