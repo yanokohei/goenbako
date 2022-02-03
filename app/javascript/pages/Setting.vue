8c09e19b96f64d5c616382e998be501ef9d21bfd
Yano Kohei <o83184206@gmail.com>
2022-02-03 16:00:45 +0900

    Add:設定ページの大枠部分の実装

--- app/javascript/pages/Setting.vue
+++ app/javascript/pages/Setting.vue
@@ -8,35 +8,54 @@
     </v-row>
     <div align="center">
       <v-card max-width="650" class="left" color="transparent" outlined>
-        <h3 class="ml-4 mt-4">
+        <h3 class="ml-4 mt-6">
           ・メール通知設定
         </h3>
         <v-divider />
       </v-card>
     </div>
-    <div align="center">
-      <v-card max-width="330" class="left" color="transparent" outlined>
-        <div class="xs-font option">
-          <p class="mt-4 ml-4">レターを受け取った際に<br>メールを受け取る</p>
-          <v-switch
-            v-model="nofitication"
-            @click="hundleNofiticationSwitch()"
-            class="xs-font switch"
-          />
-        </div>
+    <div align="center" class="mt-4">
+      <v-card max-width="310" class="left" color="transparent" outlined>
+        <validation-observer
+          ref="observer"
+          v-slot="{ invalid }"
+        >
+          <form @submit.prevent="hundleUpdateUserSetting(setting)">
+            <validation-provider
+              v-slot="{ errors }"
+              rules="email_required"
+            >
+              <v-text-field
+                v-model="user.email"
+                :error-messages="errors"
+                label="Email"
+                required
+                placeholder="例)goenbako1@example.com"
+              />
+            </validation-provider>
+            <div class="xs-font option mb-4">
+              <p class="mt-2 ml-4">レターを受け取った際に<br>メールを受け取る</p>
+              <v-switch
+                v-model="user.email_nofitication"
+                class="xs-font switch"
+              />
+            </div>
+            <v-row class="justify-center mb-5 pa-4">
+              <v-btn
+                color="green"
+                class="white--text m-font"
+                rounded
+                large
+                :disabled="invalid"
+              >
+                <v-icon>mdi-check</v-icon>
+                <span class="m-font white--text ml-2">設定を保存する</span>
+              </v-btn>
+            </v-row>
+          </form>
+        </validation-observer>
       </v-card>
     </div>
-    <v-row class="justify-center mb-5 pa-4">
-      <v-btn
-        color="green"
-        class="white--text m-font"
-        rounded
-        large
-      >
-        <v-icon>mdi-check</v-icon>
-        <span class="m-font white--text ml-2">設定を保存する</span>
-      </v-btn>
-    </v-row>
   </v-container>
 </template>
 
@@ -52,15 +71,30 @@ export default {
   },
   data () {
     return {
-      nofitication: true,
+      user: {
+        email: "",
+        email_nofitication: true,
+      },
     }
   },
   computed: {
     ...mapGetters({ currentUser: "users/currentUser" }),
   },
   methods: {
-    hundleNofiticationSwitch() {
-      console.log(this.nofitication);
+    hundleUpdateUserSetting() {
+      axios
+        .patch((`/api/users/${this.currentUser.id}`), { setting: this.user })
+        .then((res) => this.$emit("update-letter", res.data));
+        this.$store.dispatch("flash/setFlash", {
+          type: "success",
+          message: "設定を更新しました。"
+        })
+        .catch((error) => {
+          this.$store.dispatch("flash/setFlash", {
+            type: 'error',
+            message: '設定を更新できませんでした',
+          })
+        })
     },
   }
 };
