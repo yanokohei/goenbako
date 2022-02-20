f88283691ff2120f5bbb43767413785b6d87e41b
Yano Kohei <o83184206@gmail.com>
2022-02-20 11:30:01 +0900

    Add:各policyファイル

---
+++ app/policies/share_image_policy.rb
@@ -0,0 +1,8 @@
+class ShareImagePolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+end
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,8 @@
+class UserPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+end
