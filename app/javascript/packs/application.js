a7c83ff23e186381e038e4262f3b6658e9f74ff3
Yano Kohei <o83184206@gmail.com>
2022-02-22 04:53:32 +0900

    Add:PWA-setting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,6 +12,12 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
+    .then(function (reg) {
+      console.log('[Companion]', 'Service worker registered!');
+    });
+}
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
