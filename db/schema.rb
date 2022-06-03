72f23ba15a363637b3e16e81e182b3f167d288d6
Yano Kohei <o83184206@gmail.com>
2022-06-03 23:00:14 +0900

    access_tokenカラム等追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_27_071119) do
+ActiveRecord::Schema.define(version: 2022_06_02_073734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2022_02_27_071119) do
     t.string "uid", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "access_token", default: "", null: false
+    t.string "access_token_secret", default: "", null: false
     t.index ["provider", "uid"], name: "index_authentications_on_provider_and_uid"
   end
 
