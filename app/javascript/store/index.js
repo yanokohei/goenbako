c5c74f465a9f094529ff55277be466f7d7aed435
Yano Kohei <o83184206@gmail.com>
2022-06-04 16:51:34 +0900

    APIリクエストが一回で済むようにフォローリストをstoreで管理する(index.js)

--- app/javascript/store/index.js
+++ app/javascript/store/index.js
@@ -1,33 +1,33 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import users from './users'
-import helpbox from './helpbox'
-import flash from './flash'
-import createPersistedState from 'vuex-persistedstate'
-import Cookies from 'js-cookie';
+import Vue from "vue";
+import Vuex from "vuex";
+import users from "./users";
+import helpbox from "./helpbox";
+import following_members from "./following_members";
+import flash from "./flash";
+import createPersistedState from "vuex-persistedstate";
+import Cookies from "js-cookie";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
     users,
     flash,
-    helpbox
+    helpbox,
+    following_members,
   },
 
   plugins: [
     createPersistedState({
       storage: {
-        key: 'goenbakoApp',
+        key: "goenbakoApp",
 
-        paths: [
-          'users.currentUser'
-        ],
-        getItem: key => Cookies.get(key),
+        paths: ["users.currentUser"],
+        getItem: (key) => Cookies.get(key),
         setItem: (key, value) =>
           Cookies.set(key, value, { expires: 14, secure: true }), // 14日間有効
-        removeItem: key => Cookies.remove(key),
+        removeItem: (key) => Cookies.remove(key),
       },
     }),
   ],
-})
+});
